a94214bd6f8744de84b906e970b39af410c52f86
Leonardo K. R. <leo@ziondev.us>
2026-02-06 08:21:13 -0300

    refactor(action): use NotificationHelper in CompileUiFileAction

--- src/main/kotlin/com/ziondev/qtui/actions/CompileUiFileAction.kt
+++ src/main/kotlin/com/ziondev/qtui/actions/CompileUiFileAction.kt
@@ -1,10 +1,8 @@
 package com.ziondev.qtui.actions
 
 import com.ziondev.qtui.services.UiCompilerService
+import com.ziondev.qtui.utils.NotificationHelper
 import com.ziondev.qtui.utils.UiFileDetector
-import com.ziondev.qtui.QtUiCompilerBundle
-import com.intellij.notification.NotificationGroupManager
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -16,32 +14,25 @@ class CompileUiFileAction : AnAction() {
     override fun getActionUpdateThread(): ActionUpdateThread {
         return ActionUpdateThread.BGT
     }
-    
+
     override fun actionPerformed(e: AnActionEvent) {
         val project: Project = e.project ?: return
-        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE) 
+        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE)
             ?: e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)?.firstOrNull()
             ?: return
-        
+
         if (UiFileDetector.isUiFile(virtualFile, project)) {
             UiCompilerService.getInstance(project).compileUiFile(virtualFile)
         } else {
-            NotificationGroupManager.getInstance()
-                .getNotificationGroup("Qt UI Compiler")
-                ?.createNotification(
-                    QtUiCompilerBundle.message("notification.title"), 
-                    QtUiCompilerBundle.message("notification.error.not.ui.file", virtualFile.name), 
-                    NotificationType.WARNING
-                )
-                ?.notify(project)
+            NotificationHelper.notUiFile(project, virtualFile.name)
         }
     }
-    
+
     override fun update(e: AnActionEvent) {
         val project = e.project
-        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE) 
+        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE)
             ?: e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)?.firstOrNull()
-            
+
         e.presentation.isEnabled = UiFileDetector.isUiFile(virtualFile, project)
         e.presentation.isVisible = true
     }
